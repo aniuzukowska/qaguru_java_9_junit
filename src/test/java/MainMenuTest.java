1155962ae83c909c4299524ab44a9349c1f3523b
Anna Zukowska <anna-zukowska@ya.ru>
2023-04-06 19:19:22 +0300

    added MethodSource

--- src/test/java/MainMenuTest.java
+++ src/test/java/MainMenuTest.java
@@ -1,8 +1,8 @@
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.*;
+
+import java.util.stream.Stream;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
@@ -34,6 +34,21 @@ public class MainMenuTest {
     @CsvFileSource(resources = "/testdata/openedPageShouldHaveTitleGivenButtonsMainMenu_3.csv")
     @ParameterizedTest(name="В меню справа выбрать вкладку {0}, проверить, что открылась страница {1}")
     public void openedPageShouldHaveTitleGivenButtonsMainMenu_3(String testData, String expectedText) {
+        Selenide.open("https://ru.wikipedia.org/wiki/Заглавная_страница");
+        $$(".vector-menu-content-list span").find(text(testData)).click();
+        $("#firstHeading").shouldHave(text(expectedText));
+    }
+
+    static Stream<Arguments> openedPageShouldHaveTitleGivenButtonsMainMenu_4() {
+        return Stream.of(
+                Arguments.of("История", "Заглавная страница: история изменений"),
+                Arguments.of("Просмотр кода", "Просмотр кода страницы Заглавная страница")
+        );
+    }
+
+    @MethodSource()
+    @ParameterizedTest(name="В меню справа выбрать вкладку {0}, проверить, что открылась страница {1}")
+    public void openedPageShouldHaveTitleGivenButtonsMainMenu_4(String testData, String expectedText) {
         Selenide.open("https://ru.wikipedia.org/wiki/Заглавная_страница");
         $$(".vector-menu-content-list span").find(text(testData)).click();
         $("#firstHeading").shouldHave(text(expectedText));
